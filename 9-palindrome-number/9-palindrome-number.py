0972176e4337c6e4761dcb6af4df96b5d4aa37ae
Julius Dsouza <jsdsz1996@gmail.com>
2021-11-12 01:39:32 -0500

    Time: 64 ms (49.04%), Space: 13.6 MB (13.30%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if(x>0):
+            ls = self.createArray(x)
+            #print(self.createArray(x))
+            i = 0
+            j = len(ls)-1
+            flag = True
+            while(i<=j):
+                if ls[i]!=ls[j]:
+                    flag = False
+                i = i+1
+                j = j-1
+        elif(x==0):
+            flag = True
+        else:
+            flag = False
+        
+        return flag
+        
+        
+    def createArray(self, num):
+        ls = []
+        while(num!=0):
+            val = num%10
+            ls.append(val)
+            num = num/10
+        ls.reverse()
+        return ls
+        
+        
