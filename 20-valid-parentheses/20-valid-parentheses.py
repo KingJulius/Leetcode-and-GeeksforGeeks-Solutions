bae477ad47010dc305e2084240d28fc4b922e57e
Julius Dsouza <jsdsz1996@gmail.com>
2021-11-27 22:24:27 -0500

    Time: 23 ms (47.03%), Space: 13.6 MB (35.21%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stk = deque()
+        
+        # If less than 2 elements
+        if len(s) < 2:
+            return False
+        for c in s:
+            if c == "(" or  c == "{" or c == "[":
+                stk.append(c)
+            elif c == ")" or  c == "}" or c == "]":
+                if len(stk) == 0:
+                    return False
+                if (stk[-1] == "(" and c == ")") or (stk[-1] == "[" and c == "]") or (stk[-1] == "{" and c == "}"):
+                    stk.pop()
+                else:
+                    stk.append(c)
+        
+        if len(stk) == 0:
+            return True
+        else:
+            return False
