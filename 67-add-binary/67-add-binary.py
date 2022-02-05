5c20e4c2f59847731a651adebd70465aee577742
Julius Dsouza <jsdsz1996@gmail.com>
2022-02-05 11:42:14 -0500

    Time: 42 ms (21.10%), Space: 13.6 MB (50.14%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        
+        return bin(int('0b' + a, 2) + int('0b' + b, 2))[2:]
