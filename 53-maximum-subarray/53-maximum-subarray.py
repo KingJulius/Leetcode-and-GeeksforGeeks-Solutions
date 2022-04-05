a77803e53b6f6df3420048c5712c9d315e504f01
Julius Dsouza <jsdsz1996@gmail.com>
2022-04-05 16:16:08 -0400

    Time: 616 ms (89.29%), Space: 25.5 MB (74.09%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxSum = nums[0]
-        currSum = 0
-        
-        for ele in nums:
-            if currSum < 0:
-                currSum = 0
-            currSum += ele
-            maxSum = max(currSum, maxSum)
-        
-        return maxSum
+        max_sum, curr_sum = nums[0], 0
+        for j in range(len(nums)):
+            curr_sum += nums[j]
+            max_sum = max(max_sum, curr_sum)
+            if curr_sum < 0:
+                curr_sum = 0    
+        return max_sum
