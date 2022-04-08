b2752948535253121415f2619b9c957bf128b355
Julius Dsouza <jsdsz1996@gmail.com>
2022-04-08 00:53:08 -0400

    Time: 35 ms (36.94%), Space: 13.5 MB (73.74%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -5,5 +5,14 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        
-        return bin(int('0b' + a, 2) + int('0b' + b, 2))[2:]
+        res, carry, tSum = "", 0, 0
+        a, b = a[::-1], b[::-1]
+        for i in range(max(len(a), len(b))):
+            c1 = int("0b" + a[i], 2) if i < len(a) else 0
+            c2 = int("0b" + b[i], 2) if i < len(b) else 0
+            tSum = c1 + c2 + carry
+            res = str(tSum%2) + res
+            carry = tSum//2
+        if carry:
+            res = str(carry) + res
+        return res
