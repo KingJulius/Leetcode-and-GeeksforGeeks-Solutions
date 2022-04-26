c39481edbbd83878b9089476629844ff0a814b88
Julius Dsouza <jsdsz1996@gmail.com>
2022-04-26 19:35:12 -0400

    Added solution - LeetHub

---
+++ Print N-bit binary numbers having more 1s than 0s - GFG/print-nbit-binary-numbers-having-more-1s-than-0s.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+import math
+class Solution:
+	def NBitBinary(self, N):
+		# code here
+        c1, c2 = 0, 0
+        res = []
+        op = ""
+        def solve(op, c1, c2, N):
+            if N==0:
+                res.append(op)
+                return
+            solve(op+"1", c1+1, c2, N-1)
+            if c2<c1:
+                solve(op+"0", c1, c2+1, N-1)
+        solve(op, c1, c2, N)
+        return res
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		ob = Solution()	
+		answer = ob.NBitBinary(n)
+		for value in answer:
+			print(value,end=" ")
+		print()
+
+
+# } Driver Code Ends
