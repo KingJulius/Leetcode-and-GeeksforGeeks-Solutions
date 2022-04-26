259f64fdb47cbfb0d6bb1aa690d3baf32791b171
Julius Dsouza <jsdsz1996@gmail.com>
2022-04-26 18:26:39 -0400

    Time: 50 ms (49.25%), Space: 14.2 MB (78.94%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # c1 is the counter for the opening brackets
+        # c2 is the counter for the closing brackets
+        c1, c2 = n, n
+        op = ""
+        res = []
+        def solve(op, c1, c2):
+            if c1 == 0 and c2 == 0:
+                res.append(op)
+                return
+            if c1==0 or c2==0:
+                if c1==0:
+                    solve(op+")", c1, c2-1)
+                else:
+                    solve(op+"(", c1-1, c2)
+            if c1 == c2 and c1!=0:
+                solve(op+"(", c1-1, c2)
+            elif c1 < c2 and c1!=0:
+                solve(op+"(", c1-1, c2)
+                solve(op+")", c1, c2-1)
+        solve(op, c1, c2)
+        return res
