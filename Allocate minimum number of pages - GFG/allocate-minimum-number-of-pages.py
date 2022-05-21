6b033042fa181b1763b4e25c8873dfa39c50107b
Julius Dsouza <jsdsz1996@gmail.com>
2022-05-21 19:36:09 -0400

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.py
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.py
@@ -19,19 +19,18 @@ class Solution:
     def findPages(self,A, N, M):
         #code here
         res = -1
-        if N < M:
-            return -1
-        tsum = 0
-        for i in range(len(A)):
-            tsum += A[i]
-        l, r = 0, tsum
-        while l <= r:
-            mid = l + (r-l)//2
-            if self.isValid(A, N, M, mid):
-                res = mid
-                r = mid - 1
-            else:
-                l = mid + 1
+        if not N < M:
+            tsum = 0
+            for i in range(len(A)):
+                tsum += A[i]
+            l, r = 0, tsum
+            while l <= r:
+                mid = l + (r-l)//2
+                if self.isValid(A, N, M, mid):
+                    res = mid
+                    r = mid - 1
+                else:
+                    l = mid + 1
         return res
                 
 
