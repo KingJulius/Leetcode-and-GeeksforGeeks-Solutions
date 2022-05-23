bccf64cdf785a292c730a57e4572cc67502c74e5
Julius Dsouza <jsdsz1996@gmail.com>
2022-05-23 17:18:30 -0400

    Time: 38 ms (63.96%), Space: 13.8 MB (68.45%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,8 +1,12 @@
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        return pow(x,n)
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            res = helper(x, n//2)
+            res *= res
+            return x*res if n%2 else res
+        res = helper(x, abs(n))
+        return res if 0 <= n else 1/res 
