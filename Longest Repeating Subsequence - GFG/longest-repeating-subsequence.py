cf4905a9b41ef61561246235e64476abafce4193
Julius Dsouza <jsdsz1996@gmail.com>
2022-06-07 23:09:14 -0400

    Added solution - LeetHub

---
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+	def LongestRepeatingSubsequence(self, s1):
+		# Code here
+		s2 = s1[:]
+		dp = [[0 for __ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+        for i in range(1, len(s1)+1):
+            for j in range(1, len(s2)+1):
+                if s1[i-1] == s2[j-1] and i != j:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+        return dp[-1][-1]
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		str = input()
+		ob = Solution()
+		ans = ob.LongestRepeatingSubsequence(str)
+		print(ans)
+
+# } Driver Code Ends
