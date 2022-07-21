b62c74a14594655dbf675a481d7bc7062b937e66
Julius Dsouza <jsdsz1996@gmail.com>
2022-07-21 01:39:03 -0400

    Time: 46 ms (82.74%), Space: 16.3 MB (5.20%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -5,4 +5,13 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        s_counter = [0] * 26
+        t_counter = [0] * 26
+        for i in range(len(s)):
+            s_counter[ord(s[i]) - ord('a')] += 1
+        for i in range(len(t)):
+            t_counter[ord(t[i]) - ord('a')] += 1
+        hmap = defaultdict(int)
+        hmap[tuple(s_counter)] += 1
+        hmap[tuple(t_counter)] += 1
+        return True if hmap[tuple(s_counter)] == 2 else False
