61b3d1f0b30bd0e030cf6472b5b2eb3f1f66f981
Julius Dsouza <jsdsz1996@gmail.com>
2022-08-25 00:35:38 -0400

    Time: 282 ms (46.63%), Space: 43.2 MB (92.15%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -1,21 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        ptrA = headA
-        ptrB = headB
-        while ptrA != ptrB:
-            if ptrA is None:
-                ptrA = headB
-            else:
-                ptrA = ptrA.next
-            if ptrB is None:
-                ptrB = headA
-            else:
-                ptrB = ptrB.next
-        return ptrA
-        
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        currA, currB = headA, headB
+        if currA is None or currB is None:
+            return None
+        while currA != currB:
+            currA = currA.next
+            currB = currB.next
+            if currA == currB:
+                return currA
+            if currA is None:
+                currA = headB
+            if currB is None:
+                currB = headA
+        return currA
+            
