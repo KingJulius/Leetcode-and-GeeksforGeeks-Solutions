4604f1c0740da3d1252e2b8ac73aeae2ce78210e
Julius Dsouza <jsdsz1996@gmail.com>
2022-09-22 14:50:20 -0400

    Time: 72 ms (11.63%), Space: 13.8 MB (97.97%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -1,50 +1,18 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    
-    def nodeCount(self, head):
-        count = 0
-        while head:
-            count += 1
-            head = head.next
-        return count
-    
-    def nodeRemoval(self, head ,pos):
-        if head and pos == 0:
-            return head.next
-        
-        ptr1 = None
-        ptr2 = head
-        
-        while pos>0:
-            ptr1 = ptr2
-            ptr2 = ptr2.next
-            pos = pos - 1
-        
-        ptr1.next = ptr2.next
-        ptr2 = None
-
-        return head
-        
-            
-            
-    
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
-        node_count = self.nodeCount(head)
-        #print(node_count)
-        pos = node_count - n
-        if pos >= 0:
-            head = self.nodeRemoval(head, pos)
-        else:
-            head = None
-        return head
-        
-    
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:   
+        tmp = ListNode(0, head)
+        l = tmp
+        r = head
+        while n>0 and r:
+            r = r.next
+            n -=1
+        while r:
+            l = l.next
+            r = r.next
+        l.next = l.next.next
+        return tmp.next
