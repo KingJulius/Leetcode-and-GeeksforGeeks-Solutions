67e7c17f4d59b62e7c42ac50436cff76ad5967d2
Julius Dsouza <jsdsz1996@gmail.com>
2022-09-22 19:13:09 -0400

    Time: 43 ms (99.17%), Space: 13.4 MB (88.93%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -10,67 +10,17 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        carry = 0
-        l3 = ListNode(0)
-        l3_tail = l3
-        while l1 and l2:
-            sum = l1.val + l2.val + carry
-            #print(sum)
-            if (sum//10 >= 1):
-                carry = sum//10
-                sum = sum % 10
-            else:
-                carry = 0
-            temp = ListNode(sum)
-            l3_tail.next = temp
-            l3_tail = l3_tail.next
-            l1 = l1.next
-            l2 = l2.next
-        
-        if l1:
-            while l1 or carry:
-                if l1 != None:
-                    sum = l1.val  + carry
-                    #print(sum)
-                    if (sum//10 >= 1):
-                        carry = sum//10
-                        sum = sum % 10
-                    else:
-                        carry = 0
-                    temp = ListNode(sum)
-                    l3_tail.next = temp
-                    l3_tail = l3_tail.next
-                    l1 = l1.next
-                else:
-                    temp = ListNode(carry)
-                    l3_tail.next = temp
-                    l3_tail = l3_tail.next
-                    break
-        elif l2:
-            while l2 or carry:
-                if l2 != None:
-                    sum = l2.val  + carry
-                    #print(sum)
-                    if (sum//10 >= 1):
-                        carry = sum//10
-                        sum = sum % 10
-                    else:
-                        carry = 0
-                    temp = ListNode(sum)
-                    l3_tail.next = temp
-                    l3_tail = l3_tail.next
-                    l2 = l2.next
-                else:
-                    temp = ListNode(carry)
-                    l3_tail.next = temp
-                    l3_tail = l3_tail.next
-                    break
-        elif carry:
-            temp = ListNode(carry)
-            l3_tail.next = temp
-            l3_tail = l3_tail.next
-
- 
-        l3 = l3.next
-        return l3
-        
+        head = ListNode(0)
+        l3 = head
+        tsum, carry = 0, 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            digit = (v1 + v2 + carry) % 10
+            carry = (v1 + v2 + carry) // 10
+            l3.next = ListNode(digit)
+            l3 = l3.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return head.next
+            
