527929a53848bca161fa2f8d394b4108c7f8e088
Julius Dsouza <jsdsz1996@gmail.com>
2022-09-26 22:56:27 -0400

    Time: 75 ms (5.05%), Space: 13.8 MB (55.45%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -1,37 +1,16 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def count(self, head):
-        count = 0
-        while head != None:
-            count += 1
-            head = head.next
-        return count
-    
-    def middleNode(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        c = self.count(head)
-        
-        if c%2 == 1:
-            c = (c+1)//2
-            while c-1 != 0:
-                head = head.next
-                c -= 1
-            
-        else:
-            c = c//2
-            while c != 0:
-                head = head.next
-                c -= 1
-        
-        
-        
-        return head
-            
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sptr, fptr = head, head
+        # Even Condition: Fptr is None
+        # Odd Condition: Fptr.nexr is None
+        while fptr and fptr.next:
+            sptr = sptr.next
+            fptr = fptr.next if fptr else None
+            fptr = fptr.next if fptr else None
+        return sptr
         
