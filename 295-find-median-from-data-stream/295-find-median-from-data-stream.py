6629a254a45aa1a766b415fd4074f89ab01d3b28
Julius Dsouza <jsdsz1996@gmail.com>
2022-10-14 21:26:46 -0400

    Time: 1381 ms (33.58%), Space: 36.2 MB (30.91%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -2,35 +2,27 @@ class MedianFinder:
 
     def __init__(self):
         self.max_heap, self.min_heap = [], []
-
+        
 
     def addNum(self, num: int) -> None:
-        heappush(self.max_heap, -1* num)
-        
-        # Maintain lesser than equal to condition
+        heappush(self.max_heap, -1*num)
+        # Smaller and Bigger Heap Condition
         if self.max_heap and self.min_heap:
             if -1*self.max_heap[0] > self.min_heap[0]:
-                val = -1* heappop(self.max_heap)
-                heappush(self.min_heap, val)
-        
-        # Checks if size is uneven
-        if len(self.max_heap)  > len(self.min_heap) + 1:
-            val = -1* heappop(self.max_heap)
-            heappush(self.min_heap, val)
-            
-        if len(self.min_heap)  > len(self.max_heap) + 1:
-            val = heappop(self.min_heap)
-            heappush(self.max_heap, -1*val)
-            
+                heappush(self.min_heap, -1*heappop(self.max_heap))
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heappush(self.min_heap, -1*heappop(self.max_heap))
+        if len(self.min_heap) > len(self.max_heap) + 1:
+            heappush(self.max_heap, -1*heappop(self.min_heap))
 
     def findMedian(self) -> float:
-        # Odd Length Condition
-        if len(self.max_heap)  > len(self.min_heap):
-            return -1*self.max_heap[0]
-        elif len(self.min_heap)  > len(self.max_heap):
+        if len(self.min_heap) == len(self.max_heap):
+            return (-1*self.max_heap[0]+self.min_heap[0])/2
+        elif len(self.min_heap) > len(self.max_heap):
             return self.min_heap[0]
         else:
-            return (-1*self.max_heap[0]+self.min_heap[0])/2
+            return -1*self.max_heap[0]
+        
 
 
 # Your MedianFinder object will be instantiated and called as such:
