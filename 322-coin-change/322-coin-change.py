7aaf5eaf3eb3107599e371237219bfb511c63cef
Julius Dsouza <jsdsz1996@gmail.com>
2022-11-12 02:07:42 -0500

    Time: 1829 ms (81.02%), Space: 14.1 MB (85.94%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,19 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [[-1 for __ in range(amount+1)] for _ in range(len(coins)+1)]        
-        for j in range(amount+1):
-            dp[0][j] = -1
+        # M1: Using DP: S.C and T.C : O(n^2)
+        #dp = [[float('inf')-1 for __ in range(amount+1)] for _ in range(len(coins)+1)]
+        #for i in range(len(coins)+1):
+        #    dp[i][0] = 0
+        #for i in range(1, len(coins)+1):
+        #    for j in range(1, amount+1):
+        #        if coins[i-1] <= j:
+        #            dp[i][j] = min(dp[i-1][j], 1+dp[i][j-coins[i-1]])
+        #        else:
+        #            dp[i][j] = dp[i-1][j]
+        #return dp[-1][-1] if dp[-1][-1] != float('inf')-1 else -1
+        
+        dp = [float('inf')-1] * (amount+1)
+        dp[0] = 0
         for i in range(1, len(coins)+1):
-            dp[i][0] = 0
-        for j in range(1, amount+1):
-            if j%coins[0] == 0:
-                dp[1][j] = j//coins[0]
-            else:
-                dp[1][j] = float("inf") - 1
-        for i in range(2, len(coins)+1):
             for j in range(1, amount+1):
                 if coins[i-1] <= j:
-                    dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j])
-                else:
-                    dp[i][j] = dp[i-1][j]
-        return dp[-1][-1] if dp[-1][-1] != float("inf")-1 else -1
+                    dp[j] = min(dp[j], 1 + dp[j-coins[i-1]])
+        return dp[-1] if dp[-1] != float('inf')-1 else -1
