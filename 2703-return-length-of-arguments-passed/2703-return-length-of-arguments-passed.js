12a147af6584a42e6d1c60c888b7f7a2738f2308
Julius Dsouza <jsdsz1996@gmail.com>
2024-06-29 19:51:31 -0700

    Time: 49 ms (71.27%), Space: 49 MB (13.26%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -3,6 +3,13 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
+    // Approach 1: S.C: O(1) & T.C: O(n)
+    let ctr = 0
+    for(let arg of args){
+        ctr += 1
+    }
+    return ctr
+    // Approach 2: S.C: O(1) & T.C: O(1)
     return args.length
 };
 
