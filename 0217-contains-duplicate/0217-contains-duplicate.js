8f461cb692027fe337e56f8e4f58dd26ff761004
Julius Dsouza <jsdsz1996@gmail.com>
2025-05-30 22:54:48 -0700

    Time: 16 ms (66.78%), Space: 73.4 MB (57.89%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let uniqueSet = new Set()
+    for (let i = 0; i < nums.length; i++) {
+        if (uniqueSet.has(nums[i])) return true
+        uniqueSet.add(nums[i])
+    } 
+    return false
+};
