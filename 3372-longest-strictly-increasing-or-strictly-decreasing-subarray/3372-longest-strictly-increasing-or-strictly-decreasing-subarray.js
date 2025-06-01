4909b4bc883ddc8ef30e486508f02aa60ea322ee
Julius Dsouza <jsdsz1996@gmail.com>
2025-06-01 14:20:13 -0700

    Time: 103 ms (5%), Space: 64.1 MB (5.5%) - LeetHub

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestMonotonicSubarray = function(nums) {
+    let incLen = 1, decLen = 1, maxLen = 1
+    for (let i = 0; i < nums.length-1; i++) {
+        if (nums[i] > nums[i+1]) {
+            decLen += 1
+            incLen = 1
+        } else if (nums[i] < nums[i+1]) {
+            incLen += 1
+            decLen = 1
+        } else {
+            incLen = 1
+            decLen = 1
+        }
+        maxLen = Math.max(incLen, decLen, maxLen)
+        console.log(incLen)
+        console.log(decLen)
+        console.log(maxLen)
+    }
+
+    return maxLen
+};
