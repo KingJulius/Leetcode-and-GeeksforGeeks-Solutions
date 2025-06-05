308d6b28aa4bf21f8b4a33b47256696eb60d8a1c
Julius Dsouza <jsdsz1996@gmail.com>
2025-06-04 22:25:43 -0700

    Time: 367 ms (5.04%), Space: 65.1 MB (5.07%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let hmap = {}
+    let l = 0, maxLen = 0
+    for (let r = 0; r < s.length; r++) {
+        hmap[s[r]] = (hmap[s[r]] || 0) + 1
+        while (r-l+1 > Object.keys(hmap).length) {
+            hmap[s[l]] -= 1
+            if (hmap[s[l]] === 0) delete hmap[s[l]]
+            l++
+        }
+        maxLen = Math.max(maxLen, r-l+1)
+        console.log(maxLen)
+    }
+    return maxLen
+};
