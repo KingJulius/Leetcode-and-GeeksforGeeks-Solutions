7a64aca2f3871af68306eff945288f020a3995e4
Julius Dsouza <jsdsz1996@gmail.com>
2025-06-07 21:43:32 -0700

    Time: 3 ms (90.78%), Space: 59.6 MB (97.59%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    let tSum = map[s[s.length - 1]]
+    let prev = s[s.length - 1]
+    for (let i = s.length-2; i >= 0; i--) {
+        if (map[s[i]] >= map[prev]) tSum += map[s[i]]
+        else tSum -= map[s[i]]
+        prev = s[i]
+    }
+
+    return tSum
+};
