092a092144abc49a68f21d2e83449a3c772a641c
Julius Dsouza <jsdsz1996@gmail.com>
2025-06-08 17:07:57 -0700

    Time: 21 ms (25.04%), Space: 59.6 MB (40.49%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.js
+++ 0224-basic-calculator/0224-basic-calculator.js
@@ -3,25 +3,26 @@
  * @return {number}
  */
 var calculate = function(s) {
+    const stk = []
     let res = 0, num = 0, sign = 1
-    const stk = [] 
-    for (let c of s) {
-        if ('0'.charCodeAt(0) <= c.charCodeAt(0) && c.charCodeAt(0) <= '9'.charCodeAt(0)) {
-            num = num * 10 + parseInt(c)
-        } else if (c === '+' || c === '-') {
+    for (let i = 0; i < s.length; i++) {
+        if ('0'.charCodeAt(0) <= s[i].charCodeAt(0) && s[i].charCodeAt(0) <= '9'.charCodeAt(0)) {
+            num = 10 * num + parseInt(s[i])
+        } else if (s[i] === '+' || s[i] === '-') {
             res += sign * num
-            sign = c === '-' ? -1 : 1
+            sign = s[i] === '+' ? 1 : -1
             num = 0
-        } else if (c === '(') {
+        } else if (s[i] === '(') {
             stk.push(res)
             stk.push(sign)
-            sign = 1
             res = 0
-        } else if (c === ')') {
+            sign = 1
+        } else if (s[i] === ')') {
             res += sign * num
             res *= stk.pop()
             res += stk.pop()
             num = 0
+            sign = 1
         }
     }
     res += sign * num
