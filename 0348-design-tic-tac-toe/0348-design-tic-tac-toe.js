b06b752826c9d4acada88b8b35c4fde1594f90a8
Julius Dsouza <jsdsz1996@gmail.com>
2025-06-08 17:36:14 -0700

    Time: 75 ms (23.45%), Space: 61.2 MB (48.28%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.js
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.js
@@ -2,9 +2,9 @@
  * @param {number} n
  */
 var TicTacToe = function(n) {
-    this.countRows = new Array(n).fill(0).map(_ => [0, 0])
-    this.countCols = new Array(n).fill(0).map(_ => [0, 0])
-    this.countDiags = [[0, 0], [0, 0]]
+    this.rowCount = new Array(n).fill(0).map(_ => [0, 0])
+    this.colCount = new Array(n).fill(0).map(_ => [0, 0])
+    this.diagCount = [[0, 0], [0, 0]]
     this.target = n
 };
 
@@ -15,30 +15,25 @@ var TicTacToe = function(n) {
  * @return {number}
  */
 TicTacToe.prototype.move = function(row, col, player) {
-    // Row
-    let rowCount = this.countRows[row]
-    rowCount[player - 1] += 1
-    if (rowCount[player - 1] === this.target) return player
+    let rC= this.rowCount[row]
+    rC[player - 1] += 1
+    if (rC[player - 1] === this.target) return player
 
-    // Cols
-    let colCount = this.countCols[col]
-    colCount[player - 1] += 1
-    if (colCount[player - 1] === this.target) return player
-    
-    // Diags
-    let diagCount
+    let cC = this.colCount[col]
+    cC[player - 1] += 1
+    if (cC[player - 1] === this.target) return player
+
     if (row === col) {
-        diagCount = this.countDiags[0]
-        diagCount[player - 1] += 1
-        if (diagCount[player - 1] === this.target) return player
+        let dC = this.diagCount[0]
+        dC[player - 1] += 1
+        if (dC[player - 1] === this.target) return player
     }
 
     if (row === this.target - 1 - col) {
-        diagCount = this.countDiags[1]
-        diagCount[player - 1] += 1
-        if (diagCount[player - 1] === this.target) return player
+        let dC = this.diagCount[1]
+        dC[player - 1] += 1
+        if (dC[player - 1] === this.target) return player
     }
-
 
     return 0
 };
