082a69c77349b74df18d91d97d27cabead2ba110
Julius Dsouza <jsdsz1996@gmail.com>
2025-06-16 23:15:39 -0700

    Time: 2 ms (87.54%), Space: 58 MB (64.89%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -4,14 +4,14 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    let l = 0, res = Infinity, currSum = 0
+    let l = 0, currSum = 0, minLen = Infinity
     for (let r = 0; r < nums.length; r++) {
         currSum += nums[r]
         while (currSum >= target) {
-            res = Math.min(res, r-l+1)
+            minLen = Math.min(minLen, r-l+1)
             currSum -= nums[l]
             l += 1
         }
     }
-    return res === Infinity ? 0 : res
+    return minLen === Infinity ? 0 : minLen
 };
