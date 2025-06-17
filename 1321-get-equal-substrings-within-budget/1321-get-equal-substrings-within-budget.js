1409a8eb4313f3e019b72f7e04184b0b52ed1896
Julius Dsouza <jsdsz1996@gmail.com>
2025-06-16 23:09:01 -0700

    Time: 12 ms (23.31%), Space: 56.2 MB (64.66%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.js
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.js
@@ -5,14 +5,15 @@
  * @return {number}
  */
 var equalSubstring = function(s, t, maxCost) {
-    let l = 0, currCost = 0, res = 0
+    let l = 0, currSum = 0, maxLen = 0
     for (let r = 0; r < s.length; r++) {
-        currCost += Math.abs(s[r].charCodeAt(0) - t[r].charCodeAt(0))
-        while (currCost > maxCost) {
-            currCost -= Math.abs(s[l].charCodeAt(0) - t[l].charCodeAt(0))
+        let diff = Math.abs(s[r].charCodeAt(0) - t[r].charCodeAt(0))
+        currSum += diff
+        while (currSum > maxCost) {
+            currSum -= Math.abs(s[l].charCodeAt(0) - t[l].charCodeAt(0))
             l += 1
         }
-        res = Math.max(res, r-l+1)
+        maxLen = Math.max(maxLen, r-l+1)
     }
-    return res
+    return maxLen
 };
