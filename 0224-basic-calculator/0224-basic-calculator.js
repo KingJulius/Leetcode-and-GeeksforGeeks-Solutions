0c70917d9dc3b7ed9d0e83950adb936efd3ffd7e
Julius Dsouza <jsdsz1996@gmail.com>
2025-06-17 01:00:55 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.js
+++ 0224-basic-calculator/0224-basic-calculator.js
@@ -6,7 +6,7 @@ var calculate = function(s) {
     let currSum = 0, res = 0, sign = 1, stk = []
     for (let i = 0; i < s.length; i ++) {
         if ('0'.charCodeAt(0) <= s[i].charCodeAt(0) && s[i].charCodeAt(0) <= '9'.charCodeAt(0)) {
-            currSum += 10 * currSum + parseInt(s[i])
+            currSum = 10 * currSum + parseInt(s[i])
         } else if (s[i] === '+' || s[i] === '-') {
             res += sign * currSum
             sign = s[i] === '-' ? -1 : 1
